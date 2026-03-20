939c84e5bfcd467ce28274c06833564c3e4bc0ba
MIGUEL ANGEL AYLLON CHINCHAY <MA420847@PRESTACLUBDMN.LOCAL>
2026-03-20 10:41:51 -0500

    prueba cambio texto para ver que se mantenga carpeta upload en los despliegues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Principal from "@/features/home/PrincipalPage";
 export const metadata: Metadata = {
   title: "PrestaClub - Financiamiento con Garantía Hipotecaria y Vehicular",
   description: "Obtén el capital que tu negocio o proyecto necesita con el respaldo de tu propiedad o vehículo. +23 años, +15,000 operaciones. Registrados en la SBS.",
-  keywords: ["préstamos hipotecarios", "garantía hipotecaria", "financiamiento Perú", "PrestaClub", "crédito vehicular"],
+  keywords: ["préstamos hipotecarios", "préstamos con garantía hipotecaria", "garantía hipotecaria", "financiamiento Perú", "PrestaClub", "crédito vehicular"],
 };
 
 export default function Home() {
